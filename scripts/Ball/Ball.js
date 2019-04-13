0b3f41bbe4ef0e014025290c027d70ebc63e80aa
Florent DUFOUR <florent@dufour.xyz>
2019-04-13 17:33:22 +0200

    Major update

--- scripts/Ball/Ball.js
+++ scripts/Ball/Ball.js
@@ -1,15 +1,16 @@
 class Ball {
 
-  constructor(){
+  constructor(category, name, url){
     // Offset along the perlin path
     this.xoff1 = random(0, 1000);
-    this.xoff2 = random(100000, 100000000);
+    this.xoff2 = random(10000, 100000);
     this.x = random(0, windowWidth);
     this.y = random(0, windowHeight);
     this.history = [];
-    this.category = "";
-    this.name = "Name of the project";
-    this.link = "";
+    this.category = category;
+    this.name = name;
+    this.url = url;
+    this.isHighlighted = false;
   }
 
   update() {
@@ -37,20 +38,8 @@ class Ball {
       var d = map(i, 0, len, 10, 30);
       var fillint = map(i, 0, len, 0, 60);
       fill(0, fillint);
-
-      //fill(map(value,start1,stop1,start2,stop));
       ellipse(pos.x, pos.y, d);
     }
     endShape();
-  }
-
-  highlight() {
-    stroke(0);
-    strokeWeight(1);
-    noFill();
-    ellipse(this.x, this.y, 70);
-    noStroke();
-    fill(0);
-    text("name", 30, 40);
   }
 }
